a530cc5968412cfa03dacdd85dc46465d0b39bc7
Guille <73083329+guille-python@users.noreply.github.com>
2022-10-06 10:36:31 -0300

    Modelos
    Creacion de las clases del modelo y regristro en AppBlog/Admin para que aparezcan en el portal de Administracion

--- ProyectoBlog/AppBlog/admin.py
+++ ProyectoBlog/AppBlog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from AppBlog.models import Cantante, Album, Concierto, Articulo
 # Register your models here.
+
+admin.site.register(Cantante)
+admin.site.register(Album)
+admin.site.register(Concierto)
+admin.site.register(Articulo)
--- ProyectoBlog/AppBlog/models.py
+++ ProyectoBlog/AppBlog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Cantante(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    fecha_nacimiento = models.DateField(null=True)
+    email = models.EmailField()
+
+    def __str__(self) -> str:
+        return f"{self.nombre} {self.apellido}"
+
+class Album(models.Model):
+    nombre = models.CharField(max_length=30)
+    cant_temas = models.IntegerField()
+    fecha_de_lanzamiento = models.DateField(null=True)
+
+    def __str__(self):
+        return f"{self.nombre}"
+
+class Concierto(models.Model):
+    nombre = models.CharField(max_length=30)
+    lugar = models.CharField(max_length=30)
+    fecha_de_concierto = models.DateField(null=True)
+
+    def __str__(self):
+        return f"{self.nombre}"
+
+class Articulo(models.Model):
+    nombre = models.CharField(max_length=30)
+    texto = models.CharField(max_length=1000)
+    fecha = models.DateField(null=True)
+
+    def __str__(self):
+        return f"{self.nombre}"
