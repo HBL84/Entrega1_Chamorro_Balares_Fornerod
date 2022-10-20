6ae3488b356acbe270819ef020f62df17efe0a74
Guille <73083329+guille-python@users.noreply.github.com>
2022-10-19 22:15:29 -0300

    Test de funciones
    Test de prueba de funcionalidades.

--- ProyectoBlog/AppBlog/tests.py
+++ ProyectoBlog/AppBlog/tests.py
@@ -6,21 +6,38 @@ from AppBlog.views import Cantante, Album, Concierto
 class ViewTestCase(TestCase):
 
     def test_crear_cantante(self):
-        Cantante.objects.create(nombre="Cantante1")
-        todos_los_cantantes = Cantante.objects.all()
-        assert len(todos_los_cantantes) == 1
-        assert todos_los_cantantes[0].nombre == "Cantante1"
+        todos_los_cantantes_pre = Cantante.objects.all()
+        cant_cantantes_pre = len(todos_los_cantantes_pre) + 1
+        Cantante.objects.create(nombre="Cantante_test")
+        todos_los_cantantes_pos = Cantante.objects.all()
+        cant_cantates_pos = len(todos_los_cantantes_pos)
+        assert cant_cantantes_pre == cant_cantates_pos
+        assert todos_los_cantantes_pos[cant_cantantes_pre-1].nombre == "Cantante_test"
 
 
     def test_crear_album(self):
-        Album.objects.create(cant_temas="10")
-        todos_los_albums = Album.objects.all()
-        assert todos_los_albums[0].fecha_de_lanzamiento is None
+        todos_los_albums_pre = Album.objects.all()
+        cant_todos_los_albums_pre = len(todos_los_albums_pre) + 1
+        Album.objects.create(nombre="album_test", cant_temas= 10)
+        todos_los_albums_pos = Album.objects.all()
+        cant_todos_los_albums_pos = len(todos_los_albums_pos)
+        assert cant_todos_los_albums_pre == cant_todos_los_albums_pos
+        assert todos_los_albums_pos[cant_todos_los_albums_pre-1].nombre == "album_test"
         
 
 
     def test_concierto(self):
-        Concierto.objects.create(lugar="Lugar_de_concierto")
-        todos_los_conciertos = Concierto.objects.all()
-        assert len(todos_los_conciertos) == 1
-        assert todos_los_conciertos[0].lugar == "Lugar_de_concierto"
+        todos_los_conciertos_pre = Concierto.objects.all()
+        cant_todos_los_conciertos_pre = len(todos_los_conciertos_pre) + 1 
+        Concierto.objects.create(nombre="Lugar_de_concierto_test")
+        todos_los_conciertos_pos = Concierto.objects.all()
+        cant_todos_los_conciertos_pos = len(todos_los_conciertos_pos)
+        assert cant_todos_los_conciertos_pre == cant_todos_los_conciertos_pos
+        assert todos_los_conciertos_pos[cant_todos_los_conciertos_pre-1].nombre == "Lugar_de_concierto_test"
+
+
+test_1 = ViewTestCase()
+
+test_1.test_crear_cantante()
+test_1.test_crear_album()
+test_1.test_concierto()
--- ProyectoBlog/AppMsj/models.py
+++ ProyectoBlog/AppMsj/models.py
@@ -4,3 +4,5 @@ from django.db import models
 # Create your models here.
 class Mensajes(models.Model):
     texto_mensaje = models.CharField(max_length=40)
+
+    class Meta: verbose_name_plural = "Mensajes"
